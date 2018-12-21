51922a84370abbaa8a7069324a9ddf0e7133bb3d
CaiKnife <caiknife@foxmail.com>
2018-12-21 12:23:44 +0800

    二分搜索

---
+++ data/binarysearch/binarysearch.go
@@ -0,0 +1,24 @@
+package binarysearch
+
+func BinarySearch(a []int, v int) int {
+	n := len(a)
+	if n == 0 {
+		return -1
+	}
+
+	low := 0
+	high := n - 1
+
+	for low <= high {
+		mid := (low + high) / 2
+		if (a[mid] == v) {
+			return mid
+		} else if a[mid] > v {
+			high = mid - 1
+		} else {
+			low = mid + 1
+		}
+	}
+
+	return -1
+}
