dd29c3034201257ee5bd9fd88019dcf7b5cb40a0
CaiKnife <caiknife@foxmail.com>
2018-12-21 14:01:37 +0800

    二分搜索查找

--- data/binarysearch/binarysearch.go
+++ data/binarysearch/binarysearch.go
@@ -1,5 +1,6 @@
 package binarysearch
 
+// 使用循环的二分查找
 func BinarySearch(a []int, v int) int {
 	n := len(a)
 	if n == 0 {
@@ -11,7 +12,7 @@ func BinarySearch(a []int, v int) int {
 
 	for low <= high {
 		mid := low + ((high - low) >> 1)
-		if (a[mid] == v) {
+		if a[mid] == v {
 			return mid
 		} else if a[mid] > v {
 			high = mid - 1
@@ -23,6 +24,7 @@ func BinarySearch(a []int, v int) int {
 	return -1
 }
 
+// 使用递归的二分查找
 func BinarySearchRecursive(a []int, v int) int {
 	n := len(a)
 	if n == 0 {
@@ -32,6 +34,7 @@ func BinarySearchRecursive(a []int, v int) int {
 	return bs(a, v, 0, n-1)
 }
 
+// 递归二分查找
 func bs(a []int, v int, low int, high int) int {
 	if low > high {
 		return -1
@@ -45,4 +48,113 @@ func bs(a []int, v int, low int, high int) int {
 	} else {
 		return bs(a, v, mid+1, high)
 	}
+}
+
+// 查找第一个等于给定值的元素
+func BinarySearchFirst(a []int, v int) int {
+	n := len(a)
+	if n == 0 {
+		return -1
+	}
+
+	low := 0
+	high := n - 1
+
+	for low <= high {
+		mid := low + ((high - low) >> 1)
+		if a[mid] > v {
+			high = mid - 1
+		} else if a[mid] < v {
+			low = mid + 1
+		} else {
+			if mid == 0 || a[mid-1] != v {
+				return mid
+			} else {
+				high = mid - 1
+			}
+		}
+	}
+
+	return -1
+}
+
+// 查找最后一个等于给定值的元素
+func BinarySearchLast(a []int, v int) int {
+	n := len(a)
+	if n == 0 {
+		return -1
+	}
+
+	low := 0
+	high := n - 1
+	for low <= high {
+		mid := low + ((high - low) >> 1)
+		if a[mid] > v {
+			high = mid - 1
+		} else if a[mid] < v {
+			low = mid + 1
+		} else {
+			if mid == n-1 || a[mid+1] != v {
+				return mid
+			} else {
+				low = mid + 1
+			}
+		}
+	}
+
+	return -1
+}
+
+// 查找第一个大于等于给定值的元素
+func BinarySearchFirstGT(a []int, v int) int {
+	n := len(a)
+	if n == 0 {
+		return -1
+	}
+
+	low := 0
+	high := n - 1
+	for low <= high {
+		mid := low + ((high - low) >> 1)
+		if a[mid] > v {
+			high = mid - 1
+		} else if a[mid] < v {
+			low = mid + 1
+		} else {
+			if mid != n-1 && a[mid+1] > v {
+				return mid + 1
+			} else {
+				low = mid + 1
+			}
+		}
+	}
+
+	return -1
+}
+
+// 查找最后一个小于等于给定值的元素
+func BinarySearchLastLT(a []int, v int) int {
+	n := len(a)
+	if n == 0 {
+		return -1
+	}
+
+	low := 0
+	high := n - 1
+	for low <= high {
+		mid := low + ((high - low) >> 1)
+		if a[mid] > v {
+			high = mid - 1
+		} else if a[mid] < v {
+			low = mid + 1
+		} else {
+			if mid == 0 || a[mid-1] < v {
+				return mid - 1
+			} else {
+				high = mid - 1
+			}
+		}
+	}
+
+	return -1
 }
