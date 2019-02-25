26e9487c58b6a3a65f4410181fdd39c91e958ab5
CaiKnife <caiknife@foxmail.com>
2019-02-25 12:05:14 +0800

    binary tree

--- data/tree/BinaryTree.go
+++ data/tree/BinaryTree.go
@@ -1,6 +1,9 @@
 package tree
 
-import "fmt"
+import (
+	"go-test/data/stack"
+	"fmt"
+)
 
 type BinaryTree struct {
 	root *TreeNode
@@ -8,4 +11,49 @@ type BinaryTree struct {
 
 func NewBinaryTree(v interface{}) *BinaryTree {
 	return &BinaryTree{NewNode(v)}
+}
+
+func (this *BinaryTree) PreOrder() {
+	p := this.root
+	s := stack.NewArrayStack()
+}
+
+func (this *BinaryTree) InOrder() {
+	p := this.root
+	s := stack.NewArrayStack()
+}
+
+func (this *BinaryTree) PostOrder() {
+	s1 := stack.NewArrayStack()
+	s2 := stack.NewArrayStack()
+}
+
+func PreOrderTraverse(node *TreeNode) {
+	p := node
+	if p == nil {
+		return
+	}
+	fmt.Sprintf("%+v ", p.data)
+	PreOrderTraverse(p.left)
+	PreOrderTraverse(p.right)
+}
+
+func InOrderTraverse(node *TreeNode) {
+	p := node
+	if p == nil {
+		return
+	}
+	InOrderTraverse(p.left)
+	fmt.Sprintf("%+v ", p.data)
+	InOrderTraverse(p.right)
+}
+
+func PostOrderTraverse(node *TreeNode) {
+	p := node
+	if p == nil {
+		return
+	}
+	PostOrderTraverse(p.left)
+	PostOrderTraverse(p.right)
+	fmt.Sprintf("%+v ", p.data)
 }
